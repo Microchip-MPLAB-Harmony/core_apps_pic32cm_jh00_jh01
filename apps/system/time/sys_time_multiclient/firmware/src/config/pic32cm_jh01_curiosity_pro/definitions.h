d2d702d1a5f3f5f718f430431fdd6198402a0d7a
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-16 12:09:50 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_pic32cm_jh01_curiosity_pro
    MH3-75911

--- apps/system/time/sys_time_multiclient/firmware/src/config/pic32cm_jh01_curiosity_pro/definitions.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32cm_jh01_curiosity_pro/definitions.h
@@ -54,13 +54,13 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/sercom/usart/plib_sercom4_usart.h"
 #include "bsp/bsp.h"
+#include "peripheral/sercom/usart/plib_sercom4_usart.h"
 #include "peripheral/tc/plib_tc0.h"
-#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -72,6 +72,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32CM5164JH01100"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "PIC32CM"
+#define DEVICE_SERIES		 "PIC32CMJH01"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 48000000
